783508ce291957f4c0731cc848483d1f11a8773c
joe <joe.culjak@gmail.com>
2021-04-09 23:05:49 -0400

    71. Styling Menu Items
    broken our homepage into smaller components, finished styling our menu-item component

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -5,12 +5,13 @@ import './menu-item.styles.scss';
 const MenuItem = ({ title, imageUrl, size }) => {
   const menuItemClass = size ? `${size} menu-item` : 'menu-item';
   return (
-    <div
-      className={menuItemClass}
-      style={{ backgroundImage: `url(${imageUrl})` }}
-    >
+    <div className={menuItemClass}>
+      <div
+        className="background-image"
+        style={{ backgroundImage: `url(${imageUrl})` }}
+      />
       <div className='content'>
-        <h1 className='title'>{title}</h1>
+        <h1 className='title'>{title.toUpperCase()}</h1>
         <span className='subtitle'>SHOP</span>
       </div>
     </div>
