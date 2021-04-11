5efef9fdbb7165c2ed262034bc9d9de25686715a
joe <joe.culjak@gmail.com>
2021-04-11 11:45:15 -0400

    120. Cart Dropdown Component, roughed in

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -6,6 +6,7 @@ import './header.styles.scss';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 import { auth } from '../../firebase/firebase.utils';
 import CartIcon from "../cart-icon/cart-icon.component";
+import CartDropdown from "../cart-dropdown/cart-dropdown.component";
 
 const Header = ({ currentUser }) => (
   <div className='header'>
@@ -22,6 +23,7 @@ const Header = ({ currentUser }) => (
       }
       <CartIcon />
     </div>
+    <CartDropdown />
   </div>
 );
 
