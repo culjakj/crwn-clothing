fb1ab412a543be19f2837dd2ebb181a988f8aefa
joe <joe.culjak@gmail.com>
2021-04-10 23:09:31 -0400

    97. Google Sign In Authentication 3, integrated firebase into application, created sign in page with sign in component and sign in with google

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -2,10 +2,13 @@ import React from 'react';
 
 import './custom-button.styles.scss';
 
-const CustomButton = ({ children, ...otherProps }) => (
-  <button className='custom-button' {...otherProps}>
-    {children}
-  </button>
-);
+const CustomButton = ({ children, isGoogleSignIn, ...otherProps }) => {
+  const buttonClass = (isGoogleSignIn ? 'google-sign-in' : '') + ' custom-button';
+  return (
+    <button className={buttonClass} {...otherProps}>
+      {children}
+    </button>
+  );
+};
 
 export default CustomButton;
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -3,15 +3,21 @@ import { Link } from "react-router-dom";
 
 import './header.styles.scss';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
+import { auth } from '../../firebase/firebase.utils';
 
-const Header = (props) => (
+const Header = ({ currentUser }) => (
   <div className='header'>
     <Link className='logo-container' to='/'>
-      <Logo className='logo'/>
+      <Logo className='logo' />
     </Link>
     <div className="options">
       <Link className="option" to='/shop'>SHOP</Link>
       <Link className="option" to='/contact'>CONTACT</Link>
+      {
+        currentUser
+          ? <div className='option' onClick={() => auth.signOut()}>SIGN OUT</div>
+          : <Link className='option' to='/signin'>SIGN IN</Link>
+      }
     </div>
   </div>
 );
