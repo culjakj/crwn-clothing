93df6961067f9a1914459fda49c4b7f60194d794
joe <joe.culjak@gmail.com>
2021-04-12 22:51:04 -0400

    137. Checkout Item Component, update cart to use selectors from reselt; misc - fixed font in index html

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -4,42 +4,45 @@ import { createStructuredSelector } from "reselect";
 
 import './checkout.styles.scss';
 import { selectCartItems, selectCartTotal } from "../../redux/cart/cart.selectors";
+import CheckoutItem from "../../components/checkout-item/checkout-item.component";
 
-const CheckoutPage = ({cartItems, total}) => {
-  return (
-    <div className='checkout-page'>
-      <div className="checkout-header">
-        <div className="header-block">
-          <span>Product</span>
+const CheckoutPage = ({ cartItems, total }) => {
+    return (
+      <div className='checkout-page'>
+        <div className="checkout-header">
+          <div className="header-block">
+            <span>Product</span>
+          </div>
+          <div className='header-block'>
+            <span>Description</span>
+          </div>
+          <div className='header-block'>
+            <span>Quantity</span>
+          </div>
+          <div className='header-block'>
+            <span>Price</span>
+          </div>
+          <div className='header-block'>
+            <span>Remove</span>
+          </div>
         </div>
-        <div className='header-block'>
-          <span>Description</span>
-        </div>
-        <div className='header-block'>
-          <span>Quantity</span>
-        </div>
-        <div className='header-block'>
-          <span>Price</span>
-        </div>
-        <div className='header-block'>
-          <span>Remove</span>
+        {
+          cartItems.map(cartItem => <CheckoutItem key={cartItem.id} cartItem={cartItem} />)
+        }
+        <div className="total">
+          <span>TOTAL: ${total}</span>
         </div>
       </div>
-      {
-        cartItems.map(cartItem => cartItem.name)
-      }
+    );
+  }
+;
 
-      <div className="total">
-        <span>TOTAL: ${total}</span>
-      </div>
-    </div>
-  );
-};
-
-const mapStateToProps = createStructuredSelector({
-  cartItems: selectCartItems,
-  total: selectCartTotal
-});
+const mapStateToProps = createStructuredSelector(
+  {
+    cartItems: selectCartItems,
+    total: selectCartTotal
+  }
+);
 
 export default connect(mapStateToProps)(CheckoutPage);
 
